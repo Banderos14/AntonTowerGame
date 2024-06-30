60a66e270b6bd35d562ae5285e38d2a954da3ca9
Oleg Dobzhenko <oleg.dovzhenko@iothink-solutions.com>
2024-06-30 14:55:16 +0200

    Fixed FPS counter, fixed pixalated canvas, added 0,0 coordinates to the center of the screen.

--- Scripts/game_core.js
+++ Scripts/game_core.js
@@ -6,6 +6,7 @@ let previousTime = 0.0;
 function start() {
     game.canvas = document.getElementById('game-canvas');
     game.context = game.canvas.getContext('2d');
+    game.context.translate(game.canvas.width/2, game.canvas.height/2);
 
     window.requestAnimationFrame(time => {
         previousTime = time;
@@ -36,20 +37,19 @@ function render() {
     // Exit if game is not initialized.
     if (game == undefined || game.context == undefined) return;
 
-    game.context.clearRect(0, 0,  game.context.canvas.width,  game.context.canvas.height);
-
-    game.context.fillStyle = "#ff00ff";
-    game.context.fillRect(0, 0,  game.context.canvas.width,  game.context.canvas.height);
+    // Clear background
+    game.context.clearRect(-game.context.canvas.width / 2, -game.context.canvas.height / 2,  game.context.canvas.width,  game.context.canvas.height);
     
-    // set the canvas origin (0,0) to center canvas
-    // All coordinates to the left of center canvas are negative
-    // All coordinates below center canvas are negative
-
-    game.context.font = "12px serif";
+    // Render FPS stats in top left corner.
+    game.context.font = "24px fantasy";
     game.context.fillStyle = "red";
-    game.context.fillText(game.fps, 10, 30);
+    game.context.fillText(game.fps, -game.context.canvas.width / 2 + 8, -game.context.canvas.height / 2 + 32);
+
+
 }
 
 function update(dt){
-    game.fps = (1000/dt).toFixed(2) + "fps"; 
+
+    // Calculate frame rate.
+    game.fps = (1000/dt).toFixed(0); 
 }
--- Scripts/main.js
+++ Scripts/main.js
@@ -1,51 +1,9 @@
 function startGame(){
     $("#start-button").css("display","none");
     $("#game-canvas").css("display","block");
-
+    var canvas = document.getElementById( "game-canvas" );
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    
     start();
-}
-
-function add(){
-
-    var a = 10;
-
-    var b = 15;
-
-
-    console.log(a + b);
-
-    a = a + b;
-
-    console.log(a + b);
-
-
-    var string1 = "Oleg";
-
-    var string2 = "Anton"
-
-
-
-    console.log(string1 + string2);
-
-
-    var c = 5;
-    
-    var d = 10;
-
-    console.log(c * d);
-
-
-    
-
-    var car = {}
-    
-    car.name = "Mustang";
-    car.engine = "v8";
-    car.speed = 200;
-
-    console.log(car.engine);
-
-    console.log(car.name);
-    
-    console.log(car.speed);
 }
